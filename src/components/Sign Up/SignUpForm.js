9f1940a69cabe9e3b3afb8a08cc28c68fd9149c8
Abdiel Cortés <zurken@Abdiels-MacBook-Pro.local>
2020-09-26 04:02:34 -0400

    added sign up button, directs to home

--- src/components/Sign Up/SignUpForm.js
+++ src/components/Sign Up/SignUpForm.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
+import history from "../../Routing/history";
+import "./SignUp.css";
+import "./SignUpForm.css";
 
 export class SignUpForm extends Component {
     render() {
         return (
             <div>
                 <h1>Form</h1>
+                <button className="signupbutton" onClick={() => history.push("/home")}>
+                    Sign Up
+                </button>
             </div>
         )
     }
 }
 
 export default SignUpForm
-    
