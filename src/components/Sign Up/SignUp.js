38d5a54744560c9898213c366e5e51f230a521d3
Abdiel Cortés <zurken@Abdiels-MacBook-Pro.local>
2020-09-26 03:09:02 -0400

    added topnav bar to sign up, links back to login

--- src/components/Sign Up/SignUp.js
+++ src/components/Sign Up/SignUp.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import history from "../../Routing/history";
+import "./SignUp.css";
 import SignUpForm from "./SignUpForm";
 
 export class SignUp extends Component {
@@ -9,19 +11,25 @@ export class SignUp extends Component {
     email: '',
     degree: '',
     password: ''
-}
-//Handle fields change
-handleChange = input => e => {
-  this.setState({[input]: e.target.value});
-}
+  }
+  //Handle fields change
+  handleChange = input => e => {
+    this.setState({[input]: e.target.value});
+  }
 
-  
   render() {
     const { firstName, lastName, studentNumber, email, degree,  password} = this.state
     const values = { firstName, lastName, studentNumber, email, degree,  password}
     return (
-      <div>
-       This is the sign up page.
+      <div className="SignUp">
+        <div className="topnav">
+          <p>
+            {" "}
+            Sign up for a UPRM account
+            <a href ="" onClick={() => history.push("/")}>Log In</a>
+          </p>
+        </div>
+       this is the sign up form
        <SignUpForm handleChange={this.handleChange} values={values} />
       </div>
     );
