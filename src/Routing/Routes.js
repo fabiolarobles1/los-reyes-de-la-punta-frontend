78a95e62a2cd192e325697de3d7ed4650c3a6390
Fabiola Robles <you@example.comfabiola.robles1@upr.edu>
2020-11-01 12:40:51 -0400

    implemented guarded routes

--- src/Routing/Routes.js
+++ src/Routing/Routes.js
@@ -1,27 +1,24 @@
 import React, { Component } from "react";
 import { Router, Switch, Route } from "react-router-dom";
-import history from './history';
-import Home from '../components/Home/Home';
-import Login from '../components/Login/Login';
-import SignUp from '../components/Sign Up/SignUp';
+import history from "./history";
+import Home from "../components/Home/Home";
+import Login from "../components/Login/Login";
+import SignUp from "../components/Sign Up/SignUp";
+import Guard from "./GuardedRoute";
 
- class Routes extends Component {
-
-    render() {
-        return (
-            // This component manages the paths to the different screens
-            <Router history={history}>
-                <Switch>
-                    <Route path="/home" exact component={Home}> 
-                    </Route>
-                    <Route path="/" exact component={Login}>
-                    </Route>
-                    <Route path="/signup" exact component={SignUp}>
-                    </Route>
-                </Switch>
-            </Router>
-        );
-    }
+class Routes extends Component {
+  render() {
+    return (
+      // This component manages the paths to the different screens
+      <Router history={history}>
+        <Switch>
+          <Guard path="/home" component={Home}></Guard>
+          <Route path="/" exact component={Login}></Route>
+          <Route path="/signup" exact component={SignUp}></Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default Routes;
